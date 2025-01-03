13c5dd8cf816cb1d56e4372eac9bc80055287586
Vhivi <david.angelelli@hotmail.fr>
2025-01-03 16:39:47 +0100

    style: Migration of plugins using the migration guide : https://evilmartians.com/chronicles/postcss-8-plugin-migration

--- lib/stylePlugins/trim.ts
+++ lib/stylePlugins/trim.ts
@@ -1,11 +1,21 @@
 import { Root } from 'postcss'
-import * as postcss from 'postcss'
 
-export default postcss.plugin('trim', () => (css: Root) => {
-  css.walk(({ type, raws }) => {
-    if (type === 'rule' || type === 'atrule') {
-      if (raws.before) raws.before = '\n'
-      if (raws.after) raws.after = '\n'
+export default () => {
+  return {
+    postcssPlugin: 'trim',
+    Once(css: Root) {
+      css.walk(({ type, raws }) => {
+        if (type === 'rule' || type === 'atrule') {
+          if (raws.before) {
+            raws.before = '\n'
+          }
+          if (raws.after) {
+            raws.after = '\n'
+          }
+        }
+      })
     }
-  })
-})
+  }
+}
+
+export const postcss = true
